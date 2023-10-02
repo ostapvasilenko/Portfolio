b2d3037898fdd715c51022615a6461b198a0d7ec
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-10-02 14:56:50 +0300

    make and adaptive section Slider

--- src/js/script.js
+++ src/js/script.js
@@ -41,3 +41,33 @@ form.addEventListener('submit', function (e) {
         form.reset(); // Очистити форму
     }
 });
+
+// Slider Works
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 60,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    on: {
+        resize: function () {
+            if (window.innerWidth < 1500 && window.innerWidth >= 992) {
+                this.params.slidesPerView = 3;
+                this.params.spaceBetween = 40;
+            } else if (window.innerWidth < 992 && window.innerWidth >= 768) {
+                this.params.slidesPerView = 2;
+                this.params.spaceBetween = 30;
+            } else if (window.innerWidth < 768 && window.innerWidth >= 576) {
+                this.params.slidesPerView = 2;
+                this.params.spaceBetween = 30;
+            } else if (window.innerWidth < 576 && window.innerWidth >= 0) {
+                this.params.slidesPerView = 1;
+            } else {
+                this.params.slidesPerView = 4;
+            }
+            this.update();
+        },
+    },
+});
